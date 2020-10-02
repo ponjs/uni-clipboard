fda5cd9e19e81a55d4100a395ca01fd544858d79
ponjs <2552658135@qq.com>
2020-10-02 18:24:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# uni-clipboard
+
+剪贴板操作插件，全平台兼容，支持设置剪贴板的内容和获取剪贴板的内容
+
+## Import
+
+### npm
+
+```Javascript
+import { setClipboardData, getClipboardData } from 'uni-clipboard'
+```
+
+### download
+
+```Javascript
+// 以下路径需根据项目实际情况
+import { setClipboardData, getClipboardData } from '@/js_sdk/uni-clipboard'
+```
+
+## API
+
+### setClipboardData(text)
+
+设置系统剪贴板的内容
+
+- 参数：`text` `<string>` 需要设置的文本
+- 返回： `<Promise>`
+  -- `resolve` 设置成功
+  -- `reject` 设置失败
+
+### getClipboardData()
+
+获取剪贴板的内容
+
+- 返回： `<Promise>`
+  -- `resolve` 获取成功后的文本
+  -- `reject` 获取失败
---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * 设置系统剪贴板的内容
+ * @param {string} data 需要设置的内容
+ * @returns {Promise} 设置状态
+ */
+export function setClipboardData(data) {
+	return new Promise((success, fail) => {
+		// #ifndef H5
+		uni.setClipboardData({ data, success, fail });
+		// #endif
+
+		// #ifdef H5
+		navigator.clipboard.writeText(data).then(success).catch(fail);
+		// #endif
+	});
+}
+
+/**
+ * 获取系统剪贴板内容
+ * @returns {Promise} 剪贴板内容
+ */
+export function getClipboardData() {
+	return new Promise((success, fail) => {
+		// #ifndef H5
+		uni.getClipboardData({ success, fail });
+		// #endif
+
+		// #ifdef H5
+		navigator.clipboard.readText().then(success).catch(fail);
+		// #endif
+	});
+}
