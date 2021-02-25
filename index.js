1c344efca49e55f9933998679a6dc2378c68d22d
ponjs <2552658135@qq.com>
2021-02-25 12:00:10 +0800

    兼容获取

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 /**
- * 设置系统剪贴板的内容
- * @param {string} data 需要设置的内容
- * @returns {Promise} 设置状态
+ * 获取系统剪贴板内容
+ * @returns {Promise<string>} 剪贴板内容
  */
-export function setClipboardData(data) {
-	return new Promise((success, fail) => {
-		// #ifndef H5
-		uni.setClipboardData({ data, success, fail });
-		// #endif
+export function getClipboardData() {
+  return new Promise((success, fail) => {
+    // #ifndef H5
+    uni.getClipboardData({
+      success: ({ data }) => success(data),
+      fail
+    })
+    // #endif
 
-		// #ifdef H5
-		// navigator.clipboard.writeText(data).then(success).catch(fail);
-		const textarea = document.createElement('textarea');
-		textarea.value = data;
-		textarea.readOnly = 'readOnly';
-		document.body.appendChild(textarea);
-		textarea.select();
-		textarea.setSelectionRange(0, data.length);
-		document.execCommand('copy');
-		textarea.remove();
-		success(data);
-		// #endif
-	});
+    // #ifdef H5
+    try {
+      navigator.clipboard.readText().then(success).catch(fail)
+    } catch (error) {
+      fail(error)
+    }
+    // #endif
+  })
 }
 
 /**
- * 获取系统剪贴板内容
- * @returns {Promise} 剪贴板内容
+ * 设置系统剪贴板的内容
+ * @param {string} data 需要设置的内容
+ * @returns {Promise<string>} 设置的内容
  */
-export function getClipboardData() {
-	return new Promise((success, fail) => {
-		// #ifndef H5
-		uni.getClipboardData({ success, fail });
-		// #endif
+export function setClipboardData(data) {
+  return new Promise((success, fail) => {
+    // #ifndef H5
+    uni.setClipboardData({ data, success, fail })
+    // #endif
 
-		// #ifdef H5
-		navigator.clipboard.readText().then(success).catch(fail);
-		// #endif
-	});
+    // #ifdef H5
+    const textarea = document.createElement('textarea')
+    textarea.value = data
+    textarea.readOnly = true
+    document.body.appendChild(textarea)
+    textarea.select()
+    textarea.setSelectionRange(0, data.length)
+    document.execCommand('copy')
+    textarea.remove()
+    success(data)
+    // #endif
+  })
 }
